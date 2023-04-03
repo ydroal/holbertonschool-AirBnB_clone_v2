87c5e1e847ed2efebd031da8114f7c4262decea4
ydroal <droal.yoko@gmail.com>
2023-04-03 04:01:23 -0700

    fix: PEP8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 def hello_HBNB():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
     return 'HBNB'
@@ -17,4 +18,3 @@ def HBNB():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
