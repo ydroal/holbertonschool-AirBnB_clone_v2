457d0fac83754991edbadd5e1de170a0b07018f9
ydroal <droal.yoko@gmail.com>
2023-04-03 06:43:17 -0700

    add: task5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''Module for starting Flask application'''
+
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    if '_' in text:
+        text = text.replace('_', ' ')
+
+    return f'C {text}'
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    if '_' in text:
+        text = text.replace('_', ' ')
+ 
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
